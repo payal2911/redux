b3fccd26ac6d750b1746f3145790a2ef3005fcd3
Payal Thawkar <payal.thawkar@appxbuild.com>
2022-10-20 19:29:37 +0530

    Done Toolkit

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -4,7 +4,9 @@ const UserProfile = () => {
   return (
     <main className={classes.profile}>
       <h2>My User Profile</h2>
+        <div>
         <h4>Payal Thawkar</h4>
+        </div>
     </main>
   );
 };
